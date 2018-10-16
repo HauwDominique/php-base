1bb887b3aa689bf3a07d9ffee5fec1c1de2fbd73
HauwDominique <hauw.dominique@gmail.com>
2018-10-16 12:37:44 +0200

    create folder date

--- 04-boucles/03-multiplication-table.php
+++ 04-boucles/03-multiplication-table.php
@@ -1,10 +1,29 @@
 <?php
 
-for (let i = 0; i<=10; i++) {
-    echo 'i </br>';
+// afficher les tables de multiplication
+
+echo '------------------------<br><br>';
+
+echo '<table>';
+
+// légende du tableau (axe des abcisses)
+
+echo 'x';
+
+for ($z = 0; $z<=10; $z++) {
+    echo $z;
 }
+echo '<br>';
 
 
+for ($i = 0; $i<=10; $i++) {
+    echo $i;
+    for ($j = 0; $j <=10; $j++) {
+        echo $j * $i.'  ';
+    }
+    echo '<br>' ;
+  }
 
+  echo '</table>';
 
 ?>
