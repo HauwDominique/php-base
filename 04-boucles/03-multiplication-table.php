5a3399e3a809da13cc706c08f7f684ea15cbbc8b
HauwDominique <hauw.dominique@gmail.com>
2018-10-16 17:26:38 +0200

    Create Excel cours  PHP

--- 04-boucles/03-multiplication-table.php
+++ 04-boucles/03-multiplication-table.php
@@ -2,28 +2,40 @@
 
 // afficher les tables de multiplication
 
-echo '------------------------<br><br>';
+echo '------------------------<br>';
 
 echo '<table>';
 
 // légende du tableau (axe des abcisses)
 
-echo 'x';
+echo '<tr>';
+echo '<td> x </td>'; //ici on affiche la croix en haut à gauche du tableau
 
-for ($z = 0; $z<=10; $z++) {
-    echo $z;
+// ici on affiche la 1ere ligne contenant les 10 chiffres à droite de la croix
+for ($x = 0; $x<=10; $x++) {
+    echo '<td>' .$x .'</td>';
 }
-echo '<br>';
-
+echo '</tr>';
 
 for ($i = 0; $i<=10; $i++) {
-    echo $i;
-    for ($j = 0; $j <=10; $j++) {
-        echo $j * $i.'  ';
+    echo '<td>' .$i .'</td>' ;//affiche le premier chiffre de la 1ere ligne (donc chiffre de la colonne)
+    for ($j = 0; $j <=10; $j++) { //commence la boucle pour calculer chaque ligne
+        echo '<td>' .$j * $i.'</td>' ; //affiche le résultat de la multiplication pour la ligne entière (sauf le 1er chiffre 1ere colonne)
     }
-    echo '<br>' ;
+    echo '</tr><br>' ;
   }
 
   echo '</table>';
+
+
+  // Copie du tableau sans mise en forme
+  // for ($i = 0; $i<=10; $i++) {
+  //   echo $i;
+  //   for ($j = 0; $j <=10; $j++) {
+  //       echo $j * $i.'  ';
+  //   }
+  //   echo '<br>' ;
+  // }
+
 
 ?>
