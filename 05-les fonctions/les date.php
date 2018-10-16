4eb9b616132abdca476b1da9dcad2d40c5d931b2
HauwDominique <hauw.dominique@gmail.com>
2018-10-16 15:49:20 +0200

    create Date file with exercice

--- 05-les fonctions/les date.php
+++ 05-les fonctions/les date.php
@@ -1,8 +1,23 @@
 <?php
 
-echo date('l d F Y').', il est ' .date('g\h i')  .' et ' .date('s') .' seconde';
+echo date('l d F Y').', il est ' .date('g\h i')  .' et ' .date('s') .' secondes <br>';
+
+// echo date ('c / ');
+// echo date ('H\h i\m s\s') .'<br>';
+// echo time() . ' - Timestamp now <br>';
+// echo $futurDay .' - Timestamp dans 10 jours<br>';
+
+// récupérer le jour qu'il sera dans 10 jours exactement.
+
+$futurDay = strtotime ('+10 day');
+echo 'Dans 10 jours nous serons ' .date('l', $futurDay) .'<br>';
+// on peut aussi faire 
+// echo 'Dans 10 jours nous serons ' .date('l', strtotime('+10 day'));
 
 
+// Trouver quel jour sera ton prochain anniversaire
+$timestampBirthday = strtotime('13 april 2019');
+echo 'Le jour de mon anniversaire sera un ' .date('l', $timestampBirthday);
 
 
 ?>
