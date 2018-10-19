7b1ea42eeb804db4b3354b87f567d3470203a569
hauw <hauw.dominique@gmail.com>
2018-10-19 14:06:23 +0200

    end exo inclusion

--- 09-inclusion/partials/footer.php
+++ 09-inclusion/partials/footer.php
@@ -1,3 +1,21 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sticky Footer Navbar Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+
+
+
 <footer class="footer">
       <div class="container">
         <span class="text-muted">Place sticky footer content here.</span>
